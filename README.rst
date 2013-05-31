b417d07405a6bccf05cdbf50a21b466f2c13c5f7
Devin Fee <devin.fee@gmail.com>
2013-05-31 10:29:25 -0700

    textinput, textarea, and password each support a required paramter. bootstrap's textinput now allows inputs to be appended/prepended. deform_jinja2 now accepts a filters ini setting. docs have been updated

--- README.rst
+++ README.rst
@@ -3,6 +3,20 @@ deform_jinja2
 
 Jinja2 templates for deform using the uni-form model
 
+The following is an example demonstrating deform_jinja2's three supported 
+settings::
+
+    deform_jinja2.i18n.domain=deform
+    deform_jinja2.template_search_path=
+        myapp:templates/forms
+        deform_jinja2:bootstrap_templates
+    deform_jinja2.filters =
+        debug = myapp.filters.debug_filter
+        route_url = pyramid_jinja2.filters:route_url_filter
+        static_url = pyramid_jinja2.filters:static_url_filter
+    
+  These settings belong in your ``app.ini`` file
+
 To run the `deformdemo <http://deformdemo.repoze.org>`_ application using the
 ``deform_jinja2`` renderer:
 
--- deform_jinja2/__init__.py
+++ deform_jinja2/__init__.py
@@ -22,7 +22,38 @@ In your app initialization::
 
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
+from jinja2.utils import import_string
 from pkg_resources import resource_filename
+
+from compat import string_types
+
+
+def splitlines(s):
+    return filter(None, [x.strip() for x in s.splitlines()])
+
+
+def maybe_import_string(val):
+    if isinstance(val, string_types):
+        return import_string(val.strip())
+    return val
+
+
+def parse_config(config):
+    """
+    Parses config values from .ini file and returns a dictionary with
+    imported objects
+    """
+    # input must be a string or dict
+    result = {}
+    if isinstance(config, string_types):
+        for f in splitlines(config):
+            name, impl = f.split('=', 1)
+            result[name.strip()] = maybe_import_string(impl)
+    else:
+        for name, impl in config.items():
+            result[name] = maybe_import_string(impl)
+    return result
+
 
 class DummyTranslator(object):
     @staticmethod
@@ -36,12 +67,14 @@ class DummyTranslator(object):
 
         return singular
 
+
 class jinja2_renderer_factory(object):
-    def __init__(self, search_paths=(), default_templates='deform_jinja2:templates',
-            translator=None, extensions=[]):
+    def __init__(self, search_paths=(),
+                 default_templates='deform_jinja2:templates',
+                 translator=None, extensions=[], filters=None):
 
         if 'jinja2.ext.i18n' not in extensions:
-           extensions.append('jinja2.ext.i18n')
+            extensions.append('jinja2.ext.i18n')
 
         self.env = Environment(extensions=extensions)
         self.env.loader = FileSystemLoader(())
@@ -49,15 +82,20 @@ class jinja2_renderer_factory(object):
         for path in search_paths:
             self.add_search_path(path)
 
-        if translator == None:
+        if translator is None:
             translator = DummyTranslator
 
-        self.env.install_gettext_callables(translator.gettext, translator.ngettext)
+        self.env.install_gettext_callables(translator.gettext,
+                                           translator.ngettext)
 
         self.add_search_path(default_templates)
 
+        filters = filters or {}
+        self.env.filters.update(filters)
+
     def add_search_path(self, path):
-        self.env.loader.searchpath.append(resource_filename(*(path.split(':'))))
+        self.env.loader.searchpath.append(
+            resource_filename(*(path.split(':'))))
 
     def __call__(self, tname, **kw):
         if not '.jinja2' in tname:
@@ -72,8 +110,12 @@ def includeme(config):
     import deform
     settings = config.registry.settings
     domain = settings.get('deform_jinja2.i18n.domain', 'deform')
-    search_path = settings.get('deform_jinja2.template_search_path', '').strip()
-    renderer = jinja2_renderer_factory(search_paths=search_path.split(), 
-            translator=PyramidTranslator(domain=domain))
-    deform.Form.set_default_renderer(renderer)
+    search_path = settings.get('deform_jinja2.template_search_path', '')
+    filters = parse_config(settings.get('deform_jinja2.filters', ''))
 
+    renderer = jinja2_renderer_factory(
+        search_paths=search_path.strip().split(),
+        translator=PyramidTranslator(domain=domain),
+        filters=filters
+    )
+    deform.Form.set_default_renderer(renderer)
